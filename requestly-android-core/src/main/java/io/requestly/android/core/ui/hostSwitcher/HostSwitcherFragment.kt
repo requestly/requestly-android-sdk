b84e6e38543e49c8018cc1a26de947bd32876447
P1V4HH2CFG <p1v4hh2cfg@P1V4HH2CFGs-MacBook-Pro.local>
2022-10-06 13:01:18 +0530

    CRUD operations completed

--- requestly-android-core/src/main/java/io/requestly/android/core/ui/hostSwitcher/HostSwitcherFragment.kt
+++ requestly-android-core/src/main/java/io/requestly/android/core/ui/hostSwitcher/HostSwitcherFragment.kt
@@ -1,32 +1,139 @@
 package io.requestly.android.core.ui.hostSwitcher
 
+import android.annotation.SuppressLint
+import android.app.AlertDialog
+import android.app.Dialog
 import android.os.Bundle
+import android.view.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.requestly.android.core.R
 import io.requestly.android.core.databinding.FragmentHostSwitcherBinding
-
+typealias OnSaveClickFnType = (startingText: String, provisionalText: String) -> Unit
 class HostSwitcherFragment : Fragment() {
 
     private lateinit var mainBinding: FragmentHostSwitcherBinding
+    private val viewModel: HostSwitcherFragmentViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         mainBinding = FragmentHostSwitcherBinding.inflate(layoutInflater)
+
         // Inflate the layout for this fragment
-        with(mainBinding) {
-            initRecyclerView()
-        }
+        initRecyclerView()
 
         return mainBinding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val provider = object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.host_switcher_fragment_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.addNew -> {
+                        loadAddNewHostSwitchItemDialog(null) { t1, t2 ->
+                            viewModel.createItem(startingText = t1, provisionalText = t2)
+                        }
+                        true
+                    }
+                    else -> {
+                        false
+                    }
+                }
+            }
+        }
+        requireActivity().addMenuProvider(provider, viewLifecycleOwner, Lifecycle.State.RESUMED)
+    }
+
     private fun initRecyclerView() {
-        mainBinding.hostSwitcherRulesRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        mainBinding.hostSwitcherRulesRecyclerView.layoutManager =
+            LinearLayoutManager(requireContext())
+        val mapper: (SwitchingRule) -> HostSwitchItemModel = {
+            HostSwitchItemModel(
+                startingText = it.startingText,
+                provisionalText = it.provisionalText,
+                isActive = it.isActive,
+                onSwitchStateChangeListener = { boolValue ->
+                    viewModel.editSwitchState(
+                        it.id,
+                        boolValue
+                    )
+                },
+                onEditClickListener = {
+                    loadAddNewHostSwitchItemDialog(it) { t1, t2 ->
+                        viewModel.editItem(t1, t2, it.id)
+                    }
+                },
+                onDeleteClickListener = {
+                    loadDeleteConfirmationDialog {
+                        viewModel.deleteItem(it.id)
+                    }
+                }
+            )
+        }
+        val items: List<HostSwitchItemModel> =
+            viewModel.rulesListLive.value?.map(mapper) ?: emptyList()
+        val adaptor = HostSwitchItemAdaptor(items)
+        viewModel.rulesListLive.observe(viewLifecycleOwner) {
+            adaptor.items = it.map(mapper)
+            adaptor.notifyDataSetChanged()
+        }
+        mainBinding.hostSwitcherRulesRecyclerView.adapter = adaptor
+    }
+
+    private fun loadAddNewHostSwitchItemDialog(
+        itemRule: SwitchingRule?,
+        onSaveClick: OnSaveClickFnType
+    ) {
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(R.layout.host_switcher_new_item_dialog)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCancelable(false)
+        dialog.show()
+
+        val saveButton = dialog.findViewById<Button>(R.id.saveButton)
+        val cancelButton = dialog.findViewById<Button>(R.id.cancelButton)
+        val startingEditText = dialog.findViewById<EditText>(R.id.startingEditText)
+        val provisionalEditText = dialog.findViewById<EditText>(R.id.provisionalEditText)
+
+        startingEditText.setText(itemRule?.startingText ?: "", TextView.BufferType.EDITABLE)
+        provisionalEditText.setText(itemRule?.provisionalText ?: "", TextView.BufferType.EDITABLE)
+        saveButton.setOnClickListener {
+            onSaveClick(startingEditText.text.toString(), provisionalEditText.text.toString())
+            dialog.hide()
+        }
+        cancelButton.setOnClickListener {
+            dialog.hide()
+        }
+    }
+
+    private fun loadDeleteConfirmationDialog(onPositiveButtonClick: () -> Unit) {
+        AlertDialog.Builder(requireActivity())
+            .setCancelable(false)
+            .setTitle("Are you sure you want to delete this?")
+            .setPositiveButton("Yes") { dialog, _ ->
+                onPositiveButtonClick()
+                dialog.cancel()
+            }
+            .setNegativeButton("No") { dialog, _ ->
+                dialog.cancel()
+            }
+            .create()
+            .show()
     }
 }
---
+++ requestly-android-core/src/main/java/io/requestly/android/core/ui/hostSwitcher/HostSwitcherFragmentViewModel.kt
@@ -0,0 +1,44 @@
+package io.requestly.android.core.ui.hostSwitcher
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.UUID
+
+
+data class SwitchingRule(var startingText: String, var provisionalText: String, var isActive: Boolean, val id: String)
+
+class HostSwitcherFragmentViewModel: ViewModel() {
+
+    private var _rulesListLive = MutableLiveData<List<SwitchingRule>>()
+    val rulesListLive: LiveData<List<SwitchingRule>> = _rulesListLive
+
+    init {
+        _rulesListLive.value = emptyList()
+    }
+
+    fun createItem(startingText: String, provisionalText: String) {
+
+        Log.d("HOST_SWITCHER", "length -> ${rulesListLive.value?.size}")
+
+        val newRule = SwitchingRule(startingText, provisionalText, true, UUID.randomUUID().toString())
+        _rulesListLive.value = _rulesListLive.value!! + newRule
+    }
+
+    fun editSwitchState(ruleId: String, newValue: Boolean) {
+        _rulesListLive.value?.find { it.id === ruleId }?.isActive = newValue
+    }
+
+    fun editItem(startingText: String, provisionalText: String, ruleId: String) {
+        val rule =  _rulesListLive.value?.find { it.id === ruleId }
+        rule?.startingText = startingText
+        rule?.provisionalText = provisionalText
+        _rulesListLive.postValue(_rulesListLive.value)
+    }
+
+    fun deleteItem(ruleId: String) {
+        val newList = _rulesListLive.value?.filter { it.id !== ruleId } ?: emptyList()
+        _rulesListLive.value = newList
+    }
+}
